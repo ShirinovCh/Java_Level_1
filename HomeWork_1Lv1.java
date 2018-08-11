663d598f09b969a7e5552cb0d509e8d908c72f4b
ShirinovCh <39744051+ShirinovCh@users.noreply.github.com>
2018-08-12 00:08:41 +0300

    method return

--- HomeWork_1Lv1.java
+++ HomeWork_1Lv1.java
@@ -1,5 +1,5 @@
 class HomeWork_1Lv1 {
-	public static void main (String[] args){
+public static void main (String[] args){
 		byte a;// create new variables
 		short b;
 		int c;
@@ -8,11 +8,17 @@ class HomeWork_1Lv1 {
 		double f;
 		boolean g;
 		char h;
+		long i;
 		a = 5; // appoint variables 
 		b = 7;
 		c = 22;
 		d = 1;
+		i = a * (b + (c / d));
 		System.out.println(" a = " + a + ", b = " + b + ", c = " + c + ", d = " + d);// show variables
+		System.out.println(" i = " + i);
+		System.out.println( add(5,7,10,2) );
 	} 
-	
+	static long add(int a, int b, int c, int d){// method return
+		return a * (b + (c / d));
+	}
 }
