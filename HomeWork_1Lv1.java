d98d1b5b98aa2c0d68bf65438277d0985b7ea28c
ShirinovCh <39744051+ShirinovCh@users.noreply.github.com>
2018-08-11 23:06:26 +0300

    Create variables

---
+++ HomeWork_1Lv1.java
@@ -0,0 +1,17 @@
+class HomeWork_1Lv1 {
+	public static void main (String[] args){
+		byte a;// create new variables
+		short b;
+		int c;
+		long d;
+		float e;
+		double f;
+		boolean g;
+		char h;
+		a = 5; // appoint variables 
+		b = 7;
+		c = 22;
+		d = 1;
+	} 
+	
+}
