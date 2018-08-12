78a97734cbb7b974f66dc48295a9a4befb60c5bc
ShirinovCh <39744051+ShirinovCh@users.noreply.github.com>
2018-08-12 19:42:36 +0300

    compare positive or negative number

--- HomeWork_1Lv1.java
+++ HomeWork_1Lv1.java
@@ -1,3 +1,10 @@
+/*
+* Homework. Java 1. Lesson 1.
+*
+*@author Chingiz Shirinov 
+*@version Aug 12 2018
+*/
+
 import java.util.Scanner; 
 
 class HomeWork_1Lv1 {
@@ -20,7 +27,7 @@ public static void main (String[] args){
 		System.out.println(" i = " + i);
 		System.out.println(calculate(5,7,22,1));
 		System.out.println(limit10to20());
-		/*System.out.println(positive_or_negative());*/
+		System.out.println(positive_or_negative());
 		System.out.println(negative_true());
 		System.out.println(helloName());
 	} 
@@ -28,26 +35,29 @@ public static void main (String[] args){
 		return a * (b + (c / d));
 	}
 	public static boolean limit10to20 (){ // calculate  2 number and compare limit
-		System.out.println("Enter two number limit from 10 to 20?");
+		/*System.out.println("Enter number 1");*/
 		Scanner scanner =  new Scanner(System.in);
+		System.out.println("Enter first number");
         int a = scanner.nextInt();
+		System.out.println("Enter second number");
 		int b = scanner.nextInt();
 		if ((a + b) >= 10 && (a + b) <= 20)
 			return true;
 		else
 			return false;
 	}
-	/*public static int positive_or_negative (String (a)){ // compare positive or negative number
-		String p = "positive";
-		String n = "negative";
-		System.out.println("Enter positive or negative number");
+	public static String positive_or_negative (){ // compare positive or negative number
+		/*String p = "positive";
+		String n = "negative";*/
+		System.out.println("Enter negative or positive number");
 		Scanner scanner =  new Scanner(System.in);
         int a = scanner.nextInt();
+		String numberString = String.valueOf(a);
 		if (a >= 0)
-			return p;
+			return "Positive " + numberString;
 		else
-			return n;
-	}*/
+			return "Negative " + numberString;
+	}
 	public static boolean negative_true (){ // compare negative number with true or false
 		System.out.println("Enter negative number");
 		Scanner scanner =  new Scanner(System.in);
