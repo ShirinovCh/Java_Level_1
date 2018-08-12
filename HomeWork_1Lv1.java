31d37b6b0f6d5868cb35e2bdc19f0fbc2039a602
ShirinovCh <39744051+ShirinovCh@users.noreply.github.com>
2018-08-12 19:54:57 +0300

    to create branch

--- HomeWork_1Lv1.java
+++ HomeWork_1Lv1.java
@@ -71,7 +71,7 @@ public static void main (String[] args){
 		System.out.println("Enter your name");
 		Scanner scanner =  new Scanner(System.in);
         String name = scanner.next();
-		return "Hello " + name + "!";
+		return "Hello " + name + "! ";
 	}
 		
 }
